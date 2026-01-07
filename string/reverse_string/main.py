8567290fce1705c113650880bdd4ffa5d8e623a0
pedrorcruzz <phrosa.dev@gmail.com>
2026-01-07 11:21:25 -0300

    feat: finish reverse string

--- string/reverse_string/main.py
+++ string/reverse_string/main.py
@@ -1,2 +1,16 @@
-def reverse_string(s):
+def reverse_string_slicing(s):
+    reversed_string = s[::-1]
+    return reversed_string
 
+
+print(reverse_string_slicing("pedro"))
+
+
+# Outra opção
+
+def reversed_string_join(s):
+    reveserd_string = ''.join(reversed(s))
+    return reveserd_string
+
+
+print(reversed_string_join("pedro"))
